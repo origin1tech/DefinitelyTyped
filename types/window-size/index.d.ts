d5cb0a6df722aa07496160284c26dce55340edf1
Origin1 Technologies <origin1tech@gmail.com>
2018-06-09 10:34:07 -0700

    Improve typings to match actual interface exposing "get" method.
    Current typings don't properly expose the "get" method which is needed to monitor size on output stream resize event.  I didn't implement the full TTY interface mainly because the source module only uses this for a deprecated method and tty itself from node isn't exposed as an interface. Saw little need in defining it but it could be done for Node v.0.5 or below if someone really desired to do so.

--- types/window-size/index.d.ts
+++ types/window-size/index.d.ts
@@ -1,12 +1,28 @@
 // Type definitions for window-size 0.2.0
 // Project: https://github.com/jonschlinkert/window-size
 // Definitions by: Pouya Kary <https://github.com/pmkary>
+//                 Origin1 Tech <https://github.com/origin1tech>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module "window-size" {
-    /** Height of the terminal window.*/
-    export const height: number;
+declare module 'window-size' {
 
-    /** Width of the terminal window.*/
-    export const width: number;
+  interface IOptions {
+    name?: string;
+    tty?: any; // ws uses tty.getWindowSize but deprecated since v.0.6.0
+  }
+
+  interface IResult {
+    width: number;
+    height: number;
+    type: string;
+  }
+
+  interface IWindowSize {
+    get?(): IResult;
+  }
+
+  const ws: IWindowSize & IResult;
+
+  export = ws;
+
 }
